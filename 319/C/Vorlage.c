f2e4a4df54fc887ec3a1146e998b98799529fbf8
Yannick Müller <117479959+YannickMu@users.noreply.github.com>
2024-01-09 11:18:01 +0100

    adding Test

--- 319/C/Vorlage.c
+++ 319/C/Vorlage.c
@@ -5,17 +5,14 @@ Autor: Yannick Müller
 */
 
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
     //Deklaration
-    int versuche = 30;
-    int ratZahl;
+
     //Eingabe
-    printf("Geben sie eine Zahl ein welche ihre Mitspieler erraten sollen: ");
-    scanf("%d", &ratZahl);
+
     //Verarbeitung & Ausgabe
-    
+
     return 0;
 }
---
+++ 319/C/multiplikation_Mueller_Yannick.c
@@ -0,0 +1,44 @@
+/*
+Aufgabe: Prüfung Modul 319 III Programmieraufgabe "Potenzen"
+Datum: 09.01.2024
+Autor: Yannick Müller
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    //Deklaration
+    int mal;
+    float zahl;
+    float eingabe;
+    //Eingabe
+    printf("Multiplikation\n");
+    printf("----------------------------------------------------------------\n\n");
+    do
+    {
+        printf("Geben Sie den Multiplikator (0=Ende ansonsten >=0) ein: ");
+        scanf("%d", &mal);
+        if (mal < 0)
+        {
+            printf("-> Der Wert muss groesser gleich 0 sein.\n");
+        }
+        if (mal == 0)
+        {
+            return -1;
+        }     
+    } while (mal < 0);
+    printf("Geben Sie den Multiplikand ein: ");
+    scanf("%f", &zahl);
+    //Verarbeitung
+    eingabe = zahl;
+    for (int i = 0; i < mal-1; i++)
+    {
+        zahl += eingabe;
+    }
+    //Ausgabe
+    printf("\n----------------------------------------------------------------");
+    printf("\n%d x %.4f = %.4f", mal, eingabe, zahl);
+    printf("\n----------------------------------------------------------------\n");
+    return 0;
+}
