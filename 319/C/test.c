c5b2402c6a5c9b86f31e0b0249f28ca20117f887
Yannick Müller <117479959+YannickMu@users.noreply.github.com>
2024-01-09 10:32:11 +0100

    Hallo

---
+++ 319/C/test.c
@@ -0,0 +1,19 @@
+/*
+Aufgabe: 6-4
+Datum: 12.12.2023
+Autor: Yannick Müller
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //Deklaration
+    
+    //Eingabe
+    
+    //Verarbeitung & Ausgabe
+    
+    return 0;
+}
